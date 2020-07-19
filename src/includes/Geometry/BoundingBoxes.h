1d33e950a39a0b282ada91b7ccd15d07a3a3b19d
alti-tude <kartik.gupta0204@gmail.com>
2020-07-19 21:45:54 +0530

    bounding box structure

--- src/includes/Geometry/BoundingBoxes.h
+++ src/includes/Geometry/BoundingBoxes.h
@@ -4,6 +4,16 @@
 #include "glm/glm.hpp"
 
 struct BoundingBox{
+    glm::vec2 point;
+    glm::vec2 size;
 
+    bool collide(const BoundingBox& b){
+        glm::vec2 right = point + size;
+        glm::vec2 b_right = b.point + b.size;
+        if( right.y > b.point.y && point.y < b_right.y && 
+
+        )
+        return true;
+    }
 };
 #endif
