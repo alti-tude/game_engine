c5c1707fe2abda11a611d3be62daf7b09fb2d0d8
Sanjana Sunil <sanjana99sunil@gmail.com>
2020-07-30 12:31:13 +0530

    correct Bounding box reference

--- src/includes/Geometry/BoundingBoxes.h
+++ src/includes/Geometry/BoundingBoxes.h
@@ -7,9 +7,9 @@ struct BoundingBox{
     glm::vec2 point;
     glm::vec2 size;
 
-    bool collide(const BoundingBox& b){
-        if( point.x + size.x < b.x || b.x + b.size.x < point.x
-            || point.y + size.y < b.y || b.y + b.size.y < point.y
+    bool collide(const BoundingBox *b){
+        if( point.x + size.x < b->point.x || b->point.x + b->size.x < point.x
+            || point.y + size.y < b->point.y || b->point.y + b->size.y < point.y
         )
             return false;
         return true;
